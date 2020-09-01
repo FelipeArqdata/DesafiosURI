c320dcbda5e53a6138bac9c18f06c7f099afe02f
Felipe Pinto <print.fpinto@gmail.com>
2020-08-31 22:05:20 -0300

    new solutions

---
+++ 1 - Iniciante/Uri 1009.py
@@ -0,0 +1,5 @@
+vendedor = str(input())
+salario = float(input())
+vendas = float(input())
+total = (vendas*0.15)+salario
+print('TOTAL = {:.2f}'.format(total))
---
+++ 1 - Iniciante/Uri1008.py
@@ -0,0 +1,6 @@
+number= int(input())
+time = int(input())
+value = float(input())
+salary = time*value
+print('NUMBER = {}'.format(number))
+print('SALARY = U$ {:.2f}'.format(salary))
