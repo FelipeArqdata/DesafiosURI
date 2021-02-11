a3d03024ad8d4204c0062ac5bb1a5912fa824096
FelipePinto <60943061+FelipeArqdata@users.noreply.github.com>
2021-02-11 15:56:02 -0300

    Create Uri1012.py

---
+++ 1 - Iniciante/Uri1012.py
@@ -0,0 +1,14 @@
+A = float(A)
+B = float(B)
+C = float(C)
+pi = 3.14159
+trian = A*C/2
+cir = pi*C**2
+trapz= (A+B)*C/2
+quadr = B**2
+retan = A*B
+print(f'TRIANGULO: {trian:.3f}')
+print(f'CIRCULO: {cir:.3f}')
+print(f'TRAPEZIO: {trapz:.3f}')
+print(f'QUADRADO: {quadr:.3f}')
+print(f'RETANGULO: {retan:.3f}')
